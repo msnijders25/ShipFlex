386d6c2fe83865c49bf412e4924bb7fa94d69f6b
Eradates <eradates@gmail.com>
2023-03-16 10:17:54 +0100

    Update Main.java

--- ShipFlex/src/Main.java
+++ ShipFlex/src/Main.java
@@ -3,3 +3,56 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+class Boot {
+    protected String boot;
+}
+class grootte{
+    protected int lengte;
+}
+class NavigatieSystem{
+    protected boolean gpsOntvanger;
+    protected boolean kaartPlotter;
+    protected boolean kompas;
+    protected boolean diepteMeter;
+    protected boolean traagheidsNavigatie;
+}
+class Motor{
+    protected boolean elektrischBinnen;
+    protected boolean elektrischBuiten;
+    protected boolean dieselBinnen;
+    protected boolean dieselBuiten;
+}
+class BrandstofTank{
+    protected int grootte;
+    protected boolean jerrycan;
+}
+class Anker{
+    protected int anker;
+}
+class Hout{
+    protected boolean iroko;
+    protected boolean mahonie;
+    protected boolean oak;
+    protected boolean plantageTeak;
+    protected boolean padoekes;
+    protected boolean maple;
+    protected boolean essen;
+    protected boolean afzelia;
+}
+class Extra{
+    protected boolean airconditioning;
+    protected boolean Sonar;
+    protected boolean extraPks;
+    protected boolean deckStoelen;
+    protected boolean koelKast;
+    protected boolean bakPlaat;
+    protected boolean binnenStoelen;
+    protected boolean zonnepanelen;
+    protected boolean peddels;
+    protected boolean opblaasbareBootje;
+    protected boolean lifeVest;
+    protected boolean luxeIntrieur;
+    protected boolean motorBootje;
+    protected boolean luxeMaterialen; 
+}
